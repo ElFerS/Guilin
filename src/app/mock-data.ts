02d684821ac55cb390613fc3cbdc4be3274fc83e
Belen Enemark <belenenemark@gmail.com>
2019-08-22 21:43:40 -0300

    modified block-image

--- src/app/mock-data.ts
+++ src/app/mock-data.ts
@@ -4,8 +4,8 @@ import { dataPlace } from './dataPlace';
 export const DATAPLACES:dataPlace[]=[
     {name:"Puntacana",image:"assets/img/sieteveinte.jpg",price:550},
     {name:"Sudafrica & Etiopia",image:"assets/img/800por470.jpg",price:550},
-    {name:"puntacana",image:"assets/img/1800por1075.jpg" ,price:550},
-    {name:"puntacana",image:"assets/img/1000por667.jpg",price:550},
-    {name:"puntacana",image:"assets/img/1200por630.jpg",price:550},
-    {name:"puntacana",image:"assets/img/1300por868.jpg",price:550}
+    {name:"Bariloche",image:"assets/img/1800por1075.jpg" ,price:550},
+    {name:"Inglaterra- Escocia",image:"assets/img/1000por667.jpg",price:550},
+    {name:"Estados Unidos",image:"assets/img/1200por630.jpg",price:550},
+    {name:"Buenos Aires",image:"assets/img/1300por868.jpg",price:550}
 ];
